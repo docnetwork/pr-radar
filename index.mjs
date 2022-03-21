bd21ff9ac3a25e4f56361a8cf87efc376a04c5c6
Nick Ronnei <nronnei@gmail.com>
2022-03-21 10:42:17 -0400

    Adds reporting by assignee and reviewer, improves output, bumps to v0.2.0
    - Fixes bug in orgs module
    - Extracts data loading to separate method
    - Adds methods for reporting by reviewer, assignee
    - Cleans up index script

--- index.mjs
+++ index.mjs
@@ -3,6 +3,10 @@ import Scanner from './lib/Scanner.mjs';
 
 const { GITHUB_API_TOKEN, ORG, TEAM,OWNER } = process.env;
 
+function logDetails({ description, data }) {
+  console.log(`\n${description}`);
+  Object.entries(data).forEach(([k, v]) => console.log(`  ${k}: ${v.length}`));
+}
 (async () => {
   try {
     const scanner = new Scanner({
@@ -11,8 +15,12 @@ const { GITHUB_API_TOKEN, ORG, TEAM,OWNER } = process.env;
       team: TEAM,
       owner: OWNER,
     });
-
-    await scanner.scanTeamPRs();
+    await scanner.fetchOrgData();
+    scanner.getPullsByMember();
+    logDetails({ description: 'People with open PRs:', data: scanner.pullsByAssignee });
+    logDetails({ description: 'People requested on reviews:', data: scanner.pullsByReviewer });
+    logDetails({ description: 'PRs by Repo:', data: scanner.getPullsByRepo() });
+    console.log('\n');
     process.exit(0)
   } catch (error) {
     console.error(error);
